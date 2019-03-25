7c6036cf5177e7c4b675dd86d885262ed0590921
Your Name <you@example.com>
2019-03-25 12:20:01 +0800

    2019.3.25/1014

--- pat/14/14.cpp
+++ pat/14/14.cpp
@@ -7,7 +7,9 @@ int main(){
   cin>>input_1>>input_2>>input_3>>input_4;
   string week[7] = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
   int count = 0;
-  for(int i = 0;i < input_1.size(); i++){
+  int min_12 = min(input_1.size(),input_2.size());
+  int min_34 = min(input_3.size(),input_4.size());
+  for(int i = 0;i < min_12; i++){
       if(input_2[i]==input_1[i]&&count == 0&&input_1[i]>='A'&input_1[i]<='G') {
         count=1;
         cout<<week[(input_1[i] - 64) - 1]<<" ";
@@ -18,17 +20,19 @@ int main(){
           cout<<'0'<<input_1[i] - '0';
           break;
         }
-        if(input_1[i]>='A'&&input_1[i]<='Z') {
+        if(input_1[i]>='A'&&input_1[i]<='N') {
           cout<<input_1[i] - 'A' + 10;
           break;
         }
     }
   }
 
-  for(int p = 0; p < input_3.size();p++){
-    if(input_3[p] == input_4[p] && input_3[p]>=97&&input_3[p]<=122){
+  for(int p = 0; p < min_34;p++){
+    if(input_3[p] == input_4[p] && ((input_3[p]>='a'&&input_3[p]<='z')
+      ||(input_3[p]>='A'&&input_3[p]<='Z'))){
       cout<<":";
       cout<<setw(2)<<setfill('0')<<p;
+      break;
     }
   }
 }
